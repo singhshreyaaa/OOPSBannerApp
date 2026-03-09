fa6476e75f941fbe89c6b4606be3b0f050a85472
singhshreyaaa <hs7762@srmist.edu.in>
2026-03-09 21:31:59 +0530

    uc4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,33 @@
-class OOPSBannerApp3 {
+public class UC4_OOPS_Banner {
     public static void main(String[] args) {
 
-        System.out.println(String.join("", "      ***      ***      *****       ***** "));
-        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
-        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
-        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
-        System.out.println(String.join("", "    **   **  **   **   **            **   "));
-        System.out.println(String.join("", "     ** **    ** **    **             **  "));
-        System.out.println(String.join("", "      ***      ***     **        *****    "));
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
 
+        String[] P = {
+                " ******  ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       "
+        };
+
+        String[] S = {
+                "  *****  ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "  *****  "
+        };
+
+        // Print OOPS banner
+        for (int i = 0; i < 5; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
+        }
     }
 }
