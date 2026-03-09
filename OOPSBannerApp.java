5d14f7aca23877f0648525fd4d74c61f29dfc5b7
singhshreyaaa <hs7762@srmist.edu.in>
2026-03-09 11:43:04 +0530

    uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,13 @@
-class OOPSBannerApp {
+class OOPSBannerApp2 {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("      *      *      **       **      ");
+        System.out.println("     * *    * *    *   *    **           ");
+        System.out.println("    *   *  *   *   *   *   **            ");
+        System.out.println("    *   *  *   *   **      **          ");
+        System.out.println("    *   *  *   *   *            *        ");
+        System.out.println("     * *    * *    *             *       ");
+        System.out.println("      *      *     *        **         ");
+
     }
 }
